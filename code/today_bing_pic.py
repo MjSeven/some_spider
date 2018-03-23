ee4f5e9267b2b27c6435ad4138b22e8061612c5d
MjSeven <33125422+MjSeven@users.noreply.github.com>
2018-03-23 11:47:04 +0800

    重写

--- code/today_bing_pic.py
+++ code/today_bing_pic.py
@@ -4,11 +4,16 @@
 # @Author  : mz (https://github.com/MjSeven)
 # @Link    : ${link}
 # @Version : $Id$
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-01-16 20:17:35
+# @Author  : mz (https://github.com/MjSeven)
+# @Link    : ${link}
+# @Version : $Id$
 
 import os
 import sys
 import time
-import json
 import requests
 from PIL import Image
 from bs4 import BeautifulSoup as bs
@@ -16,14 +21,23 @@ from bs4 import BeautifulSoup as bs
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-ISTIMEFORMAT = '%Y-%m-%d'
-
-ti = time.strftime(ISTIMEFORMAT, time.localtime())
-
 pic_folder = "F:/Python Code/biying"
 video_folder = "F:/Python Code/biying"
-img_urls = "F:/Python Code/biying/img_urls.txt"
+info_loc = "F:/Python Code/biying/img_urls.txt"
 
+bing_url = "http://bing.com/HPImageArchive.aspx?\
+format=js&idx=0&n=1&nc=1439260838289&pid=hp&video=1"
+
+def get_request(url, stream=None):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'}
+    if stream:
+        result = requests.get(url,headers=headers, stream=True)
+        return result
+    result = requests.get(url,headers=headers)
+    return result.json()
+
+def build_url(base_url, url):
+    return base_url + url
 
 def get_pic_info():
     # 获取图片介绍
@@ -41,73 +55,41 @@ def get_pic_info():
     info = html.find("div", id="hplaSnippet")
     return info.string.strip()
 
+def info_in_file(url):
+    info_text = get_pic_info()
+    with open(info_loc, 'a') as info_file:
+        info_file.write('\n' + url + '\n')
+        info_file.write(info_text + ti)
+    print 'The info is OK!'
+
+def is_have_file(img_file):
+    if os.path.isfile(img_file):
+        return True
+    return False
+
+def build_file_name(folder, name):
+    ISTIMEFORMAT = '%Y-%m-%d'
+    ti = time.strftime(ISTIMEFORMAT, time.localtime())
+    return folder + '/' + ti + name
 
 def get_pic():
-    bing_url = "http://bing.com/HPImageArchive.aspx?\
-format=js&idx=0&n=1&nc=1439260838289&pid=hp&video=1"
-    content = requests.get(bing_url)
-    result = json.loads(content.text)
-
-    wallpaper_url = result['images'][0]['url']
-    pic_name = wallpaper_url.split('/')[-1]
-    if wallpaper_url.find('http') < 0:
-        # 存在图片网址，并将网址以及图片介绍写入 txt
-        wallpaper_url = "http://www.bing.com/" + wallpaper_url
-        # print wallpaper_url
-        info_text = get_pic_info()
-        with open(img_urls, "a") as img_urls_file:
-            img_urls_file.write('\n' + wallpaper_url + '\n')
-            img_urls_file.write(info_text + ti)
-    img_src = requests.get(wallpaper_url, stream=True)
-
-    img_file = pic_folder + "/" + ti + pic_name
-
-    # 判断图片存在并保存
-    if os.path.isfile(img_file):
-        print "The picture is already exists!"
-    else:
-        with open(img_file, 'wb') as f:
-            f.write(img_src.content)
-        print 'The picture is Ok'
-    # 显示图片
-    img = Image.open(img_file)
-    img.show()
-    return result
-
-
-def tryDownloadVideo(result):
-    # 下载视频
-    vid = result['images'][0]
-    if 'vid' in vid.keys():
-        src = vid['vid']['sources'][1]
-        video_url = src[2]
-        if not video_url:
-            return False
-
-        video_url = video_url.replace('\\', '')
-        video_name = video_url.split('/')[-1]
-
-        if video_url.find('http') > 0:
-            video_src = requests.get(video_url).content()
+    content = get_request(bing_url)
+    if 'images' in content:
+        pic_short_url = content['images'][0]['url']
+        pic_name = pic_short_url.split('/')[-1]
+        pic_full_url = 'https://www.bing.com{}'.format(pic_short_url)
+        # print pic_full_url
+        pic_file = build_file_name(pic_folder, pic_name)
+        print pic_file
+        if is_have_file(pic_file):
+            print 'The {} is already exists!'.format(pic_name)
         else:
-            video_src = requests.get('http:' + video_url).content()
-
-        video_file = video_folder + '/' + ti + video_name
-
-        if os.path.isfile(video_file):
-            return "The video is already exists!"
-        else:
-            print "The video is downloading now..."
-            with open(video_file, 'wb') as f:
-                f.write(video_src)
-            with open(img_urls, "a") as img_urls_file:
-                img_urls_file.write("\n")
-                img_urls_file.write(video_url)
-                img_urls_file.write("\n")
-            return 'The video is ok!'
-    return "There is no video to download!"
-
+            img_src = get_request(pic_full_url, True)
+            with open(pic_file, 'wb') as f:
+                f.write(img_src.content)
+            print 'The {} is OK!'.format(pic_name)
+            info_in_file(pic_full_url)
 
 if __name__ == '__main__':
-    result = get_pic()
-    tryDownloadVideo(result)
+    get_pic()
+
