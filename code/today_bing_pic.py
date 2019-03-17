be6a9851c5b468766778ffc913f553c9757fa13e
MjSeven <33125422+MjSeven@users.noreply.github.com>
2019-03-17 17:25:28 +0800

    更新
    必应最新格式返回，导致之前代码解析出错

--- code/today_bing_pic.py
+++ code/today_bing_pic.py
@@ -15,11 +15,11 @@ from PIL import Image
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-pic_folder = "xxx/Python Code/biying"
-video_folder = "xxx/Python Code/biying"
-info_loc = "xxx/Python Code/biying/img_urls.txt"
+pic_folder = "F:/Python Code/biying"
+video_folder = "F:/Python Code/biying"
+info_loc = "F:/Python Code/biying/img_urls.txt"
 
-bing_url = ("http://bing.com/HPImageArchive.aspx?"
+bing_url = ("https://cn.bing.com/HPImageArchive.aspx?"
 "format=js&idx=0&n=1&nc=1439260838289&pid=hp&video=1")
 
 ISTIMEFORMAT = '%Y-%m-%d'
@@ -31,27 +31,27 @@ def get_request(url, stream=None):
         result = requests.get(url,headers=headers, stream=True)
         return result
     result = requests.get(url,headers=headers)
-    print (result.text)
+    #print (result.text)
     return result.json()
 
 def build_url(base_url, url):
     return base_url + url
 
-def get_pic_info():
-    # 获取图片介绍
-    req = requests.get('http://cn.bing.com/')
-    ig = req.text.split('IG:"')[1].split('"')[0]
-    # print ig
-    data = {
-        'ensearch': '0', 'IID': 'SERP.5044', 'IG': ig
-    }
-    zeq = requests.get('http://cn.bing.com/cnhp/life', data)
-    # with open("biying2.html", "wb") as f:
-    #     f.write(z.text)
+# def get_pic_info():
+#     # 获取图片介绍
+#     req = requests.get('http://cn.bing.com/')
+#     ig = req.text.split('IG:"')[1].split('"')[0]
+#     # print ig
+#     data = {
+#         'ensearch': '0', 'IID': 'SERP.5044', 'IG': ig
+#     }
+#     zeq = requests.get('http://cn.bing.com/cnhp/life', data)
+#     # with open("biying2.html", "wb") as f:
+#     #     f.write(z.text)
 
-    html = bs(zeq.text, "lxml")
-    info = html.find("div", id="hplaSnippet")
-    return info.string.strip()
+#     html = bs(zeq.text, "lxml")
+#     info = html.find("div", id="hplaSnippet")
+#     return info.string.strip()
 
 def info_in_file(url):
     info_text = get_pic_info()
@@ -73,22 +73,9 @@ def get_pic():
     content = get_request(bing_url)
     if 'images' in content:
         pic_short_url = content['images'][0]['url']
-        pic_name = pic_short_url.split('/')[-1]
-        pic_full_url = 'https://www.bing.com{}'.format(pic_short_url)
+        pic_name = pic_short_url.split('&')[0].split('.',1)[-1]
+        pic_full_url = 'https://cn.bing.com{}'.format(pic_short_url)
         print (pic_full_url)
-        # video_url = content['images'][0]['vid']['sources']
-        # if video_url[0]:
-        #     #print video_url[0][2]
-        #     video_url = 'http:'+ video_url[0][2]
-        #     #print video_url
-        #     video_file = pic_folder + '' + ti + video_url.split('/')[-1]
-        #     if is_have_file(video_file):
-        #         print 'The {} is already exists!'.format(video_file)
-        #     else:
-        #         r = requests.get(video_url)
-        #         with open(video_file, 'wb') as f:
-        #             f.write(r.content)
-        #    print 'The video is Ok!'
         pic_file = pic_folder + '/' + ti + pic_name
         print (pic_file)
         if is_have_file(pic_file):
@@ -98,7 +85,7 @@ def get_pic():
             with open(pic_file, 'wb') as f:
                 f.write(img_src.content)
             print ('The {} is OK!'.format(pic_name))
-            info_in_file(pic_full_url)
+            #info_in_file(pic_full_url)
         show_image(pic_file)
         time.sleep(2)
 
